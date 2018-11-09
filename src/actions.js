f2cc01953f63b47c1b3ce07c111fa25204b286ad
talexcrowell <talexcrowell@gmail.com>
2018-11-09 11:51:06 -0800

    added actions, reducers, store

---
+++ src/actions.js
@@ -0,0 +1,38 @@
+export const ADD_GUESS = 'ADD_GUESS';
+export function addGuess(guess){
+  return{
+    type: ADD_GUESS,
+    guess
+  };
+}
+
+export const SET_FEEDBACK= 'SET_FEEDBACK';
+export function setFeedback(){
+  return{
+    type:SET_FEEDBACK 
+  }
+}
+
+export const RESTART_GAME = 'RESTART_GAME';
+export function restartGame(){
+  return {
+    type: RESTART_GAME
+  }
+}
+
+export const SET_AURAL = 'SET_AURAL';
+export function setAural(){
+  return{
+    type: SET_AURAL
+  }
+}
+
+
+// TESTING REDUCER RESPONSE
+export const END_GAME = 'END_GAME';
+export function endGame(){
+  return {
+    type: END_GAME
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import store from './store';
+import * as actions from './actions';
+
 import './reset.css';
 import './index.css';
 
 import Game from './components/game';
 
+console.log(store.getState());
+store.dispatch(actions.addGuess(5));
+console.log(store.getState());
+store.dispatch(actions.setFeedback());
+console.log(store.getState());
+store.dispatch(actions.restartGame());
+console.log(store.getState());
+store.dispatch(actions.endGame());
+console.log(store.getState());
+store.dispatch(actions.setAural());
+console.log(store.getState());
+store.dispatch(actions.addGuess(5));
+store.dispatch(actions.setAural());
+console.log(store.getState());
+
+
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+
+
